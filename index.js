2c95e2c7cdc5f541b46efc67faf86dc8c9eba556
sukhadiatej99 <57688669+sukhadiatej99@users.noreply.github.com>
2020-04-16 23:09:48 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,49 +1,163 @@
 let express = require('express');
 let app = express();
 let http = require('http').createServer(app);
+// let Sensor = require('./models/sensor.js');
 let io = require('socket.io')(http);
+const accountSid = 'AC04bd5100137a6cb05e681fa323327bd1';
+const authToken = '7c2624f730c2ad3528a3bc1c26b67adf';
+const client = require('twilio')(accountSid, authToken);
+
+
 
 app.use(express.static("./public"));
+
+// var mongoose = require('mongoose');
+// mongoose.connect('mongodb+srv://tejs:tej121232@cluster0-8sdw7.mongodb.net/hms?retryWrites=true&w=majority', function(err){
+// 	if(err) {
+// 		console.log('connection error:', err)
+// 	} else {
+// 		console.log('connection successful')
+// 	}
+// },{useNewUrlParser: true}
+// );
+
+// let db = mongoose.connect;
 
 http.listen(3000, function(){
     console.log(`server is running at:
      http://localhost:3000`);
 });
 
+// const SerialPort = require('serialport');
+// const Readline = SerialPort.parsers.Readline;
+// const port = new SerialPort('COM8'); //Connect serial port to port COM3. Because my Arduino Board is connected on port COM3. See yours on Arduino IDE -> Tools -> Port
+// const parser = port.pipe(new Readline({ delimiter: '\r\n' }));
+
+// var n = 0;
+
+// parser.on('data', (temp) => { //Read data
+//     //console.log(temp);
+//     var val = temp.split(",");
+//     num++;
+//     console.log(num);
+//     if (num >= 2880) {// arduino is sending data after every 5 sec if recieved 2880 data sets that mean 4 hour has passed so saving data after every 4 in the data base.
+//         num = 0;
+//         let dat = new Sensor();
+//         dat.temperature = val[0];
+//         dat.gas = val[1];
+//         dat.light = val[2];
+//         dat.save(err => {
+//             if (err) {
+//                 console.log(err);
+//             } else {
+//                 console.log('sensor data is saved on database.')
+//             }
+//         });
+// 	}
+// 	var today = new Date();
+// 	io.sockets.emit('temp', { date: today.getDate() + "-" + today.getMonth() + 1 + "-" + today.getFullYear(), time: (today.getHours()) + ":" + (today.getMinutes()), temp: val[0], ph: val[1], tbd: val[2] });
+// });
+
 let five = require('johnny-five');
 let arduino = new five.Board();
-let temp;
+let temp,photoresistor;
 let light_pin_led;
 const { Board, Led } = require("johnny-five");
-const board = new Board();
+const board = new Board()
 
 arduino.on('ready', function(){
 
-    var gas = new five.Sensor("A0");
+	const led = new Led.RGB({
+		pins: {
+		  red: 6,
+		  green: 5,
+		  blue: 3
+		}
+	  });
 
-  gas.scale(0, 100).on("change", function() {
+	  led.on();
+  				led.color("#008000");
+
+	var gas = new five.Sensor("A1");
+	var alarm = new five.Piezo(9);
+
+
+  	gas.scale(0, 100).on("change", function() {
+		if (this.value > 20) {
+			if (!alarm.isPlaying) {
+			  alarm.frequency(five.Piezo.Notes.a5, 5000);
+			  led.on();
+  				led.color("#FF0000");
+			}
+			//this will send sms to the user if gas is more then 20
+			client.messages
+ 			 .create({
+     			body: 'Dangerous gas level',
+     			from: '+18596956358',
+     			to: '+14379812722'
+   })
+  .then(message => console.log(message.sid));
+		}
+			else
+			{
+				led.on();
+  				led.color("#008000");
+  				
+			}
+		  
       console.log(this.value);
-    if (this.value > 60) {
-        console.log(this.value);
-
-        board.on("ready", () => {
-            const led = new Led(13);
-            board.repl.inject({
-              led
-            });
-          
-            led.blink();
-          });
-    }
-  });
-    temp = new five.Thermometer({
+	});
+	temp = new five.Thermometer({
         controller: 'LM35', 
-        pin: 'A1',
-        freq: 5000
-    });
+        pin: 'A0',
+		freq: 5000
+		
+	});
+	photoresistor = new five.Sensor({
+		pin: "A2",
+		freq: 5000
+	  });
 
 
-    temp.on('data', function(){
-        io.sockets.emit('temp', this.celsius);
-    })
-})
+	temp.on('data', function(){
+
+		if (this.celsius > 40) {
+			if (!alarm.isPlaying) {
+			  alarm.frequency(five.Piezo.Notes.a5, 5000);
+			  led.on();
+  				led.color("#FF0000");
+			}
+			//this will send sms to the user if the temperature is more then 40
+			client.messages
+			.create({
+				body: 'Dangerous temperature level',
+				from: '+18596956358',
+				to: '+14379812722'
+			  })
+		}
+			else
+			{
+				led.on();
+  				led.color("#008000");
+  				
+			}
+
+		console.log(this.celsius);
+        io.sockets.emit('tempReading', this.celsius);
+	})
+	
+	
+
+	gas.on('data', function(){
+        io.sockets.emit('gasReading', this.value);
+	})
+	photoresistor.on("data", function() {
+		console.log(this.value);
+		io.sockets.emit('photoReading', this.value);
+	});
+
+	//this will refresh all sensors after 10 seconds
+	arduino.samplingInterval(10000);
+
+});
+
--- public/main.js
+++ public/main.js
@@ -1,9 +1,98 @@
 console.log("Linked up");
 
 let socket = io.connect("http://localhost:3000");
-let therm = document.querySelector("#thermometer");
+let therm1 = document.querySelector("#temp1");
+let therm2 = document.querySelector("#temp2");
+let therm3 = document.querySelector("#temp3");
 
-socket.on('temp', function(temp){
-	console.log(temp);
-	therm.innerHTML = temp;
+let gas = document.querySelector("#somke")
+let photo1 = document.querySelector("#light1");
+let photo2 = document.querySelector("#light2");
+let x = document.querySelector("#warn");
+
+socket.on('tempReading', function(tempReading){
+	console.log(tempReading);
+	if(tempReading <= 30 )
+	{
+		therm1.innerHTML = tempReading + "°C";
+		therm2.innerHTML = "";
+		therm3.innerHTML = "";
+	}
+	else if(tempReading > 30 && tempReading <= 38 )
+	{
+		therm2.innerHTML = tempReading + "°C";
+		therm1.innerHTML = "";
+		therm3.innerHTML = "";
+	}
+	else if(tempReading > 38 )
+	{
+		therm3.innerHTML = tempReading + "°C";
+		therm2.innerHTML = "";
+		therm1.innerHTML = "";
+	}
+	if(tempReading < 30) {
+		x.style.color = '#76ef40';
+		document.querySelector("#warn").innerHTML = "Your house is safe.";
+	}
+	else if(tempReading >= 30 && tempReading < 38){
+		x.style.color = 'yellow';
+		document.querySelector("#warn").innerHTML = "Your House may be at risk";
+	}
+	else if(tempReading >= 38){
+		x.style.color = 'Red';
+		document.querySelector("#warn").innerHTML = "Your house is in danger";
+	}
+});
+
+socket.on('gasReading', function(gasReading){
+	// console.log(gasReading);
+	var sr = Math.round(gasReading);
+	//smoke.innerHTML = Math.round(gasReading);
+
+
+	if(sr <= 27 )
+	{
+		smoke1.innerHTML = "Normal";
+		smoke2.innerHTML = "";
+		smoke3.innerHTML = "";
+	}
+	else if(sr > 27 && sr <= 32 )
+	{
+		smoke2.innerHTML = "High";
+		smoke1.innerHTML = "";
+		smoke3.innerHTML = "";
+	}
+	else if(sr > 32 )
+	{
+		smoke3.innerHTML = "Very High";
+		smoke2.innerHTML = "";
+		smoke1.innerHTML = "";
+	}
+
+	if(gasReading < 27) {
+		x.style.color = '#76ef40';
+		document.querySelector("#warn").innerHTML = "Your house is safe.";
+	}
+	else if(gasReading >= 27 && gasReading < 32){
+		x.style.color = 'yellow';
+		document.querySelector("#warn").innerHTML = "Your House may be at risk";
+	}
+	else if(gasReading >= 32){
+		x.style.color = 'Red';
+		document.querySelector("#warn").innerHTML = "Your house is in danger";
+	}
+});
+
+socket.on('photoReading', function(photoReading){
+	console.log("photo",photoReading);
+	if(photoReading < 500 )
+	{
+		photo1.innerHTML = "Good";
+		photo2.innerHTML = "";
+	}
+	else if(photoReading > 500 )
+	{
+		photo2.innerHTML = "Bad";
+		photo1.innerHTML = "";
+	}
 });
