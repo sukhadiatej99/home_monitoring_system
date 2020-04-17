403a6cce30711d98ae89b4410187900c7e978249
sukhadiatej99 <57688669+sukhadiatej99@users.noreply.github.com>
2020-04-16 23:40:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ let app = express();
 let http = require('http').createServer(app);
 // let Sensor = require('./models/sensor.js');
 let io = require('socket.io')(http);
-const accountSid = 'AC04bd5100137a6cb05e681fa323327bd1';
-const authToken = '7c2624f730c2ad3528a3bc1c26b67adf';
+const accountSid = 'ACCOUNT_SID';
+const authToken = 'AUTH_TOKEN';
 const client = require('twilio')(accountSid, authToken);
 
 
@@ -93,8 +93,8 @@ arduino.on('ready', function(){
 			client.messages
  			 .create({
      			body: 'Dangerous gas level',
-     			from: '+18596956358',
-     			to: '+14379812722'
+     			from: 'FROM_NUMBER',
+     			to: 'NUMBER_YOU_WANT_TO_SEND_MESSAGE'
    })
   .then(message => console.log(message.sid));
 		}
@@ -131,8 +131,8 @@ arduino.on('ready', function(){
 			client.messages
 			.create({
 				body: 'Dangerous temperature level',
-				from: '+18596956358',
-				to: '+14379812722'
+				from: 'FROM_NUMBER',
+				to: 'NUMBER_YOU_WANT_TO_SEND_MESSAGE'
 			  })
 		}
 			else
